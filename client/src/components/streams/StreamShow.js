838e3cfc80f33699d12af56f569f1d14e20ae09a
zamorai <35697970+zamorai@users.noreply.github.com>
2020-09-26 15:39:26 -0400

    added streaming service possiblity

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,16 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 function StreamShow(props) {
 
+  const ref = useRef();
+
   useEffect(() => {
     props.fetchStream(props.match.params.id)
+    buildPlayer();
+  }, []);
 
-  }, [])
+  const buildPlayer = () => {
+    if(!props.stream || flvPlayer) {
+      return
+    }
+    var flvPlayer = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${props.match.params.id}.flv`
+    });
+
+    flvPlayer.attachMediaElement(ref.current);
+    flvPlayer.load();
+
+    return () => {
+     flvPlayer.destroy();
+    }
+
+  }
 
   const renderItems = !props.stream ? <div>Loading...</div> : (
     <div>
+      <video ref = {ref} style = {{ width: '100%'}} controls  />
       <h1>{props.stream.title}</h1>
       <h5>{props.stream.description}</h5>
     </div>
